6e2471284c10cb9b46ee92f16908abff80bfee22
caominhtri070105 <caominhtri070105@gmail.com>
2025-09-10 17:01:14 +0700

    Init for Ex4

--- LAB1_Exercise/Core/Inc/main.h
+++ LAB1_Exercise/Core/Inc/main.h
@@ -70,6 +70,12 @@ void Error_Handler(void);
 #define W_YELLOW_LED_GPIO_Port GPIOA
 #define W_GREEN_LED_Pin GPIO_PIN_7
 #define W_GREEN_LED_GPIO_Port GPIOA
+#define a_Pin GPIO_PIN_0
+#define a_GPIO_Port GPIOB
+#define b_Pin GPIO_PIN_1
+#define b_GPIO_Port GPIOB
+#define c_Pin GPIO_PIN_2
+#define c_GPIO_Port GPIOB
 #define E_RED_LED_Pin GPIO_PIN_8
 #define E_RED_LED_GPIO_Port GPIOA
 #define E_YELLOW_LED_Pin GPIO_PIN_9
@@ -82,6 +88,14 @@ void Error_Handler(void);
 #define N_YELLOW_LED_GPIO_Port GPIOA
 #define N_GREEN_LED_Pin GPIO_PIN_13
 #define N_GREEN_LED_GPIO_Port GPIOA
+#define d_Pin GPIO_PIN_3
+#define d_GPIO_Port GPIOB
+#define e_Pin GPIO_PIN_4
+#define e_GPIO_Port GPIOB
+#define f_Pin GPIO_PIN_5
+#define f_GPIO_Port GPIOB
+#define g_Pin GPIO_PIN_6
+#define g_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
